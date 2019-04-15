57c71e933ec82b932c03ad79d548d675e63ef715
Jinke.Li <1359518268@qq.com>
2019-04-15 11:14:52 +0800

    fix

--- test/index.test.js
+++ test/index.test.js
@@ -16,12 +16,12 @@ describe('test array last value babel plugin', () => {
       plugins: [babelArrayLastValuePlugin]
     })
 
-    expect(code.trimAll()).toEqual(`const arr = [1,2,3];const v = [arr.length - 1];`.trimAll())
+    expect(code.trimAll()).toEqual(`const arr = [1,2,3];const v = arr[arr.length - 1];`.trimAll())
   })
   it('test assignment', () => {
     const _code = `
       const arr = [1,2,3];
-      arr[-1] = 2
+      arr[-1] = 2;
     `
     const { code } = transform(_code, {
       plugins: [babelArrayLastValuePlugin]
